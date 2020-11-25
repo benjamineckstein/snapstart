f611326fe94b3962ed48fd6ae2847b94b3f30435
Benjamin Eckstein <>
2020-11-25 09:36:38 +0100

    add more questions

--- src/data/data_questions.js
+++ src/data/data_questions.js
@@ -47,25 +47,94 @@ export const topic_questions = [
   [3, 3, 'What is an EXPLAIN command and when do you need this?'],
 
   //4 Distributed Systems
+  [4, 0, 'What is software monitoring?'],
+  [4, 0, 'What is resilence?'],
+  [4, 0, 'What is meant by messaging in distributed systems?'],
+  [4, 1, 'What is a typical resilence pattern?'],
+  [4, 2, 'What is loose coupling? What are the benefits and how to archieve this?'],
+  [4, 2, 'What is Kafka?'],
+  [4, 2, 'What is the difference between vertical and horizontal scaling?'],
+  [4, 2, 'What is RabbitMQ?'],
+  [4, 2, 'What is an event-driven backbone and why should you use it?'],
+  [4, 2, 'What is an api gateway and why should you use it?'],
+  [4, 2, 'What is chaos engineering?'],
+
   //5 Architecture & Design
+  [5, 0, 'What is a microservice architecture?'],
+  [5, 0, 'What is a monolith?'],
+  [5, 1, 'What is a monolith and when should you use this design?'],
+  [5, 1, 'What is domain driven design?'],
+  [5, 1, 'What is the onion architecture?'],
+  [5, 1, 'What does a typical 3-tier layered architecture mean?'],
+
   //6 Tooling
+  [6, 0, 'What is git?'],
+  [6, 0, 'What does "git clone" do?'],
+  [6, 0, 'What does "git add" do?'],
+  [6, 1, 'What is JMeter?'],
+  [6, 1, 'What is Postman?'],
+  [6, 1, 'What is Gatling?'],
+  [6, 1, 'What is Prometheus?'],
+  [6, 2, 'What is the difference between git and svn?'],
+
+
   //7 APIs and Protocols
+  [7, 0, 'What is HTTP mean and stand for?'],
+  [7, 0, 'What is REST mean and stand for?'],
+  [7, 0, 'What is JSON?'],
+  [7, 0, 'What is XML?'],
+  [7, 0, 'What is SOAP?'],
+  [7, 1, 'What is HATEOAS?'],
+  [7, 1, 'What is API versioning?'],
+  [7, 1, 'What is contract testing?'],
+  [7, 1, 'What is swagger?'],
+  [7, 1, 'What is semantic versioning?'],
+  [7, 2, 'What is GrapQL?'],
 
   //8 Testing & Test Automation
   [8, 0, 'What is TDD?'],
+  [8, 0, 'What is testcoverage?'],
   [8, 0, 'Can you explain the testing pyramid?'],
   [8, 1, 'What are TestContainers?'],
+  [8, 1, 'What does a high testcoverage imply and what not?'],
   [8, 1, 'What is the difference between unit and integration tests?'],
   [8, 1, 'What are mocks and when to use them?'],
   [8, 1, 'What is the difference between JUnit 4 and 5?'],
   [8, 1, 'What are parameterized tests?'],
   [8, 1, 'What is the purpose of testing software?'],
   [8, 2, 'What is a @Testfactory in JUnit 5?'],
-  [8, 3, 'What is a @Testfactory in JUnit 5?'],
+  [8, 2, 'What is propertybased testing?'],
+  [8, 3, 'What is a dynamic testing?'],
+  [8, 3, 'What are the benefits of mutation testing?'],
 
   //9 Frontend Development
   //10 Agile Methods
+  [10, 0, 'What is Scrum?'],
+  [10, 0, 'What is the agile manifesto?'],
+  [10, 0, 'What are scrum events?'],
+  [10, 0, 'What is the difference betweend review and refinement?'],
+  [10, 0, 'What is a retro?'],
+  [10, 2, 'Please cite the agile manifesto.'],
+  [10, 2, 'What is the summary of management 3.0?'],
+  [10, 2, 'What is a PO?'],
+  [10, 2, 'What is a scrummaster?'],
+  [10, 3, 'What is servant leadership?'],
+  [10, 4, 'Please explain briefly the transition from management 1.0 to 2.0 to 3.0'],
+
   //11 Software Craft
+  [11, 0, 'What is the DRY principle?'],
+  [11, 0, 'What is the KISS principle?'],
+  [11, 0, 'What is clean code?'],
+  [11, 0, 'What is technical debt?'],
+  [11, 0, 'What is refactoring?'],
+  [11, 0, 'What is a code review?'],
+  [11, 1, 'What is the goal of a code review?'],
+  [11, 1, 'How do you determine that some part of an application needs a refactoring?'],
+  [11, 1, 'What is the SOLID principle?'],
+  [11, 2, 'What is incremental development?'],
+  [11, 2, 'What is mob programming? What are the benefits?'],
+  [11, 2, 'What is pair programming? What are the pros and contras?'],
+
   //12 Documentation
   //13 Java DI based frameworks
   //14 Performance
